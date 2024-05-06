92b4f9475020d295f1491e885303162e55d183f8
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-05-06 18:32:11 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Por favor, digite seu nome: ");
+        var nome = Console.ReadLine();
+        Console.WriteLine($"\n\nOlá, {nome}! Seja muito bem-vindo!");
+
+        Console.WriteLine("\n\nDigite qualquer tecla para sair...");
+        Console.ReadKey(true);
+    }
+}
