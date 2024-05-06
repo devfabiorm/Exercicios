fa1258ce6347052a930a7899815b1c0bcb042eef
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-05-06 20:02:03 -0300

    Exercicio5

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite a placa do carro: ");
+        var placa = Console.ReadLine();
+
+        Console.WriteLine(ValidadorDePlaca(placa) ? "Verdadeiro" : "Falso");
+    }
+
+    static bool ValidadorDePlaca(string placa)
+    {
+        if (placa == null)
+        {
+            return false;
+        }
+
+        return Regex.IsMatch(placa, "[a-zA-Z]{3}-?[0-9]{4}");
+    }
+}
