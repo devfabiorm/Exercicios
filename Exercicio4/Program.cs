1cf085869202456e50c01dba39832d14277093b4
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-05-06 19:46:33 -0300

    Exercicio 4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite algum texto: ");
+        var texto = Console.ReadLine();
+
+        int contador = 0;
+
+        for (int i = 0; i < texto?.Length; i++)
+        {
+            if (texto[i] == ' ')
+                continue;
+
+            contador++;
+        }
+
+        Console.WriteLine($"A quantidade de caracteres válidos (não espaços) digitados é: {contador}");
+    }
+}
