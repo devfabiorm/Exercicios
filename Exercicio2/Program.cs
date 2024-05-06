5548fcee0adba6d1cc246d57af0143ea65a22044
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-05-06 18:41:48 -0300

    Segundo exercício

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Por favor, digite seu primeiro nome: ");
+        var nome = Console.ReadLine();
+
+        Console.Write("Agora, por favor, digite seu sobrenome: ");
+        var sobrenome = Console.ReadLine();
+
+        Console.WriteLine($"\n\nSeu nome completo é: {nome} {sobrenome}");
+
+        Console.WriteLine("\n\nDigite qualquer tecla para sair...");
+        Console.ReadKey(true);
+    }
+}
